df3b4875640c7c64cee09633ffdc5ed2ce64710b
pblgllgs <pbl.gllgs@gmail.com>
2022-07-19 12:39:08 -0400

    multi next.config.js with deepmerge package

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const {withFrameworkConfig} = require("./framework/common/config");
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = withFrameworkConfig({
+    reactStrictMode: true,
+    i18n: {
+        locales: ['en', 'es'],
+        defaultLocale: 'en',
+    },
+});
+
+module.exports = nextConfig;
+
+console.log("next.config.js",JSON.stringify(nextConfig,null,2));

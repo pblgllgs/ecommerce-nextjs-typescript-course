f83957492fcb1afac2990b71d48f525046c5363a
pblgllgs <pbl.gllgs@gmail.com>
2022-08-04 09:05:22 -0400

    url, queery

--- framework/shopify/cart/use-add-item.ts
+++ framework/shopify/cart/use-add-item.ts
@@ -4,13 +4,17 @@ import { MutationHook } from '@common/types/hooks'
 export default useAddItem
 
 export const handler: MutationHook = {
-    fetcher: (input: any) => {
-        return JSON.stringify(input) + "_MODIFIED"
+    fetcher: async ({ fetch, input }) => {
+        const response = await fetch({
+            url: "https://backend-graphql-shopy.herokuapp.com/graphql",
+            query: ` query { hello }`
+        })
+        return response
 
     },
     useHook: ({ fetch }) => {
-        return (input: any) => {
-            const response = fetch(input)
+        return async (input: any) => {
+            const response = await fetch(input)
             return {
                 output: response
             }
